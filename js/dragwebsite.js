45a4067bd268a160c13b62cbe30732fcde6ee629
mgrztwnk5@gmail.com <mgrztwnk5@gmail.com>
2021-05-17 08:12:15 +0200

    changing

--- js/dragwebsite.js
+++ js/dragwebsite.js
@@ -1,40 +1,105 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const el = document.getElementById('texture-container');
-    el.style.cursor = 'grab';
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
 
-    let position = {top:0, left: 0, x:0, y:0 };
+// Debug
+const gui = new dat.GUI()
 
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
-    const mouseSownHandler = function(e) {
-        el.style.cursor = 'grabbing';
-        el.style.userSelect = 'none';
+// Scene
+const scene = new THREE.Scene()
 
-        position = {
-            left : el.scrollLeft,
-            top: el.scrollTop,
-            x : e.clientX,
-            y : e.clientY,
-        };
+// Objects
+const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
 
-        document.addEventListener('mousemove', mouseMoveHandler);
-        document.addEventListener('mouseup', mouseUpHandler);
-    };
+// Materials
 
-    const mouseMoveHandler = function(e) {
-        const dx = e.clientX - position.x;
-        const dy = e.clientY - position.y;
+const material = new THREE.MeshBasicMaterial()
+material.color = new THREE.Color(0xff0000)
 
-        el.scrollTop = position.top - dy;
-        el.scrollLeft = position.left - dx;
-    };
+// Mesh
+const sphere = new THREE.Mesh(geometry,material)
+scene.add(sphere)
 
-    const mouseUpHandler = function(){
-        el.style.cursor = 'grab';
-        el.style.removeProperty('user-select');
+// Lights
 
-        document.removeEventListener('mousemove', mouseMoveHandler);
-        document,removeEventListener('mouseup', mouseUpHandler);
-    };
+const pointLight = new THREE.PointLight(0xffffff, 0.1)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
 
-    el.addEventListener('mousedown', mouseDownHandler);
-});
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    sphere.rotation.y = .5 * elapsedTime
+
+    // Update Orbital Controls
+    // controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
